b4199096142db3e3264a9c0fa32894b978ea3941
ryt1s <rytisj27@gmail.com>
2025-09-15 00:47:23 +0300

    Add get student information

--- main.cpp
+++ main.cpp
@@ -23,18 +23,46 @@ struct Student {
 
 int main()
 {
-    int n, suma = 0, laik_paz;
-    Student stud;
-    cout << "Kuo vardu studentas(-e)?"; cin >> stud.var;
-    cout << "Kokia jo (jos) pavarde? "; cin >> stud.pav;
-    cout << "Kiek pazymiu ivesite? "; cin >> n;
-     for (auto i = 0; i<n; i++) {
-        cout << i+1 << "-asis pazymys: "; cin >> laik_paz;
-        stud.paz.push_back(laik_paz);
-        suma+=laik_paz;
-    }
-    cout << "Koks egzamino ivertinimas? "; cin >> stud.egz;
-    stud.gal = 0.4 * double(suma)/double(n) + 0.6 * stud.egz;
+    vector<Student> studentai;
+    char testi;
+
+    do {
+        int suma = 0, laik_paz;
+        Student stud;
+        cout << "Kuo vardu studentas(-e)?"; cin >> stud.var;
+        cout << "Kokia jo (jos) pavarde? "; cin >> stud.pav;
+        cout << "Iveskite namu darbu pazymius (0 - baigti):" << endl;
+
+        while(true) {
+            cin >> laik_paz;
+            if (laik_paz == 0) break;
+            
+            stud.paz.push_back(laik_paz);
+            suma += laik_paz;
+        }
+
+        cout << "Koks egzamino ivertinimas? ";
+        cin >> stud.egz;
+        
+        if (!stud.paz.empty()) {
+            stud.gal = 0.4 * (double)suma / stud.paz.size() + 0.6 * stud.egz;
+        } else {
+            stud.gal = 0.6 * stud.egz;
+        }
+
+        studentai.push_back(stud);
+
+        cout << "Ar norite ivesti dar viena studenta? (t/n):";
+        cin >> testi;
+    } while(testi == 't' || testi == 'T');
+
+    //  for (auto i = 0; i<n; i++) {
+    //     cout << i+1 << "-asis pazymys: "; cin >> laik_paz;
+    //     stud.paz.push_back(laik_paz);
+    //     suma+=laik_paz;
+    // }
+    // cout << "Koks egzamino ivertinimas? "; cin >> stud.egz;
+    // stud.gal = 0.4 * double(suma)/double(n) + 0.6 * stud.egz;
 
     cout << setw(15) << left << "Pavarde"
          << setw(10) << left << "Vardas"
@@ -43,9 +71,7 @@ int main()
 
     cout << "----------------------------------------" << endl;
 
-    cout << setw(15) << left << stud.pav
-         << setw(10) << left << stud.var 
-         << fixed << setprecision(2) << stud.gal << endl;
+//     cout << setw(15) << left << stud.pav
+//          << setw(10) << left << stud.var 
+//          << fixed << setprecision(2) << stud.gal << endl;
 }
-
-
